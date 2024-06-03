bc72f7c920e37410db5d8713d12e7dc9dd3a6b1f
bongani10 <bonganigadlela@gmail.com>
2024-06-03 07:51:14 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ArraysE4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] i = { 5, 2, 8, 1 }; //declare and initiliase
+
+            Console.WriteLine("Length is " + i.Length);
+
+            for (int j = 0; j < i.Length; j++)
+            {
+                Console.WriteLine(i[j]);
+            }
+            
+        }//end main
+
+    }//end class
+
+}//end namespace
